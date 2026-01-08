6425dbb78f05a18bb5ebf7a824ac4e245accd220
kwiinnn <solomon23charles@gmail.com>
2026-01-08 22:29:26 +0800

    Added autoplay to rocj paper scissors, used forEach instead of for loop in todolist

--- scripts/rockpaperscissors.js
+++ scripts/rockpaperscissors.js
@@ -25,6 +25,25 @@ let score = JSON.parse(localStorage.getItem('score')) ||
 		return computerMove;
 	}
 
+	let isAutoPlaying = false;
+	let intervalId;
+
+	function autoPlay() {
+
+		if (!isAutoPlaying) {
+			intervalId = setInterval(function() {
+				const playerMove = pickComputerMove();
+				playGame(playerMove);
+			}, 1000)
+			isAutoPlaying = true;
+		}
+		else {
+			clearInterval(intervalId);
+			isAutoPlaying = false;
+		}
+	}
+
+
 	function playGame (playerMove) {
 		
 	const computerMove = pickComputerMove();
--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -4,19 +4,18 @@ function renderTodoList() {
 
 	let todoListHTML = '';
 
-	for (let i = 0; i < todoList.length; i++) {
-		const todoObject = todoList[i];
+	todoList.forEach(function (todoObject, index) {
 		const { name, dueDate } = todoObject;
 		const html = `
 			<div>${name}</div>
 			<div>${dueDate}</div>
 			<button class="delete-button" onclick="
-				todoList.splice(${i}, 1);
+				todoList.splice(${index}, 1);
 				renderTodoList();
 			">Delete
 			</button>`;
 		todoListHTML += html;
-	}
+	} )
 
 	document.querySelector('.js-todo-list')
 		.innerHTML = todoListHTML;
